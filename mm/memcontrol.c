f3e51561a091796e269c501369f6935e0084654c
Dmitry Torokhov <dtor@chromium.org>
2016-10-06 15:53:38 -0700

    CHROMIUM: remove Android's cgroup generic permissions checks
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Change-Id: I2f780a5992c34e52a8f2d0b3557fc9d490da2779
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6021,11 +6021,6 @@ static int mem_cgroup_can_attach(struct cgroup_taskset *tset)
 	return ret;
 }
 
-static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
-{
-	return subsys_cgroup_allow_attach(tset);
-}
-
 static void mem_cgroup_cancel_attach(struct cgroup_taskset *tset)
 {
 	mem_cgroup_clear_mc();
@@ -6192,10 +6187,6 @@ static int mem_cgroup_can_attach(struct cgroup_taskset *tset)
 {
 	return 0;
 }
-static int mem_cgroup_allow_attach(struct cgroup_taskset *tset)
-{
-	return 0;
-}
 static void mem_cgroup_cancel_attach(struct cgroup_taskset *tset)
 {
 }
@@ -6229,7 +6220,6 @@ struct cgroup_subsys memory_cgrp_subsys = {
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
 	.attach = mem_cgroup_move_task,
-	.allow_attach = mem_cgroup_allow_attach,
 	.bind = mem_cgroup_bind,
 	.legacy_cftypes = mem_cgroup_files,
 	.early_init = 0,
