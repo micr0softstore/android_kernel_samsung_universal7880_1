efc4121450e6cdaf47c317bc2349189876e56b74
Daniel Borkmann <daniel@iogearbox.net>
2017-07-02 02:13:25 +0200

    BACKPORT: bpf, net: add skb_mac_header_len helper
    Add a small skb_mac_header_len() helper similarly as the
    skb_network_header_len() we have and replace open coded
    places in BPF's bpf_skb_change_proto() helper. Will also
    be used in upcoming work.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1922,10 +1922,16 @@ static inline unsigned char *skb_mac_header(const struct sk_buff *skb)
 	return skb->head + skb->mac_header;
 }
 
+static inline u32 skb_mac_header_len(const struct sk_buff *skb)
+{
+        return skb->network_header - skb->mac_header;
+}
+
 static inline int skb_mac_header_was_set(const struct sk_buff *skb)
 {
 	return skb->mac_header != (typeof(skb->mac_header))~0U;
 }
+
 
 static inline void skb_reset_mac_header(struct sk_buff *skb)
 {
@@ -1937,6 +1943,7 @@ static inline void skb_set_mac_header(struct sk_buff *skb, const int offset)
 	skb_reset_mac_header(skb);
 	skb->mac_header += offset;
 }
+
 
 static inline void skb_pop_mac_header(struct sk_buff *skb)
 {
