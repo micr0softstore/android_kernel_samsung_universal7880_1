5f8da03647e967a86f1fff4e5649e530586a3876
Chenbo Feng <fengc@google.com>
2022-11-26 17:05:28 -0700

    Missed additions from a4e86d997cb6c New getsockopt option to get socket cookie
    Change-Id: I6cce3132dd5de2078c0790d86535cc10d6d05a61

--- net/core/sock.c
+++ net/core/sock.c
@@ -1130,6 +1130,7 @@ int sock_getsockopt(struct socket *sock, int level, int optname,
 
 	union {
 		int val;
+		u64 val64;
 		struct linger ling;
 		struct timeval tm;
 	} v;
@@ -1360,6 +1361,12 @@ int sock_getsockopt(struct socket *sock, int level, int optname,
 		v.val = sk->sk_incoming_cpu;
 		break;
 
+	case SO_COOKIE:
+		lv = sizeof(u64);
+		if (len < lv)
+			return -EINVAL;
+		v.val64 = sock_gen_cookie(sk);
+		break;
 	default:
 		return -ENOPROTOOPT;
 	}
