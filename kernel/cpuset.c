77a3a3111ea39da83e8a5653427bdd8c5bd853e4
Derek <nielson925@gmail.com>
2022-11-29 17:47:28 -0700

    Missed cgroup and kernfs changes
    Changes now required from the following upstream commits
    d29c1b5be - kernfs: make kernfs_path*() behave in the style of strlcpy()
    0ed2e6461 - cgroup: make cgroup_path() and friends behave in the style of strlcpy()
    46628c4ce - cgroup: kill cgrp_ss_priv[CGROUP_CANFORK_COUNT] and friends
    
    Change-Id: Icebd50b12a11f7bd41be89af683c390555da7738

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2777,7 +2777,7 @@ void __cpuset_memory_pressure_bump(void)
 int proc_cpuset_show(struct seq_file *m, struct pid_namespace *ns,
 		     struct pid *pid, struct task_struct *tsk)
 {
-	char *buf, *p;
+	char *buf;
 	struct cgroup_subsys_state *css;
 	int retval;
 
@@ -2789,17 +2789,17 @@ int proc_cpuset_show(struct seq_file *m, struct pid_namespace *ns,
 	retval = -ENAMETOOLONG;
 	rcu_read_lock();
 	css = task_css(tsk, cpuset_cgrp_id);
-	p = cgroup_path(css->cgroup, buf, PATH_MAX);
+	retval = cgroup_path(css->cgroup, buf, PATH_MAX);
 	rcu_read_unlock();
-	if (!p)
+	if (retval >= PATH_MAX)
 		goto out_free;
-	seq_puts(m, p);
+	seq_puts(m, buf);
 	seq_putc(m, '\n');
 	retval = 0;
 out_free:
 	kfree(buf);
 out:
-	return retval;
+	return 0;
 }
 #endif /* CONFIG_PROC_PID_CPUSET */
 
